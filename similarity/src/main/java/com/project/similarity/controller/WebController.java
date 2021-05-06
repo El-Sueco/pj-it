6122b9e607fd7bf7e742f1719399e680e057998a
Sven Stauder <svenstauder@outlook.at>
2021-05-06 13:14:06 +0200

    web routing

--- similarity/src/main/java/com/project/similarity/controller/WebController.java
+++ similarity/src/main/java/com/project/similarity/controller/WebController.java
@@ -8,8 +8,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class WebController {
 
     @RequestMapping(path="/", method=RequestMethod.GET)
+    public String about(){
+        return "about";
+    }
+
+    @RequestMapping(path="/database", method=RequestMethod.GET)
     public String database(){
         return "database";
+    }
+
+    @RequestMapping(path="/check-two", method=RequestMethod.GET)
+    public String checkTwo(){
+        return "check2";
+    }
+
+    @RequestMapping(path="/check-more", method=RequestMethod.GET)
+    public String checkMore(){
+        return "check-more";
+    }
+
+    @RequestMapping(path="/manual", method=RequestMethod.GET)
+    public String manual(){
+        return "manual";
     }
 
 }
